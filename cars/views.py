6f1bb8d4c9b3d7467f21339d9242242785f89c69
joao <vitordossantos99@hotmail.com>
2023-12-04 22:06:55 -0300

    Inserção de templates base

--- cars/views.py
+++ cars/views.py
@@ -6,8 +6,8 @@ from cars.models import Car
 def cars_view(request):
     search = request.GET.get('search')
 
-    cars =  Car.objects.all()
+    cars =  Car.objects.all().order_by('model')
 
     if search:
-        cars = Car.objects.filter(model__contains=search)
+        cars = Car.objects.filter(model__icontains=search)
     return render(request,'cars.html', {'cars': cars })
