da259bb98da6def6f4f95a0be38a086ea586eba7
joao <vitordossantos99@hotmail.com>
2023-12-07 20:33:16 -0300

    adição de forms.py e alterações para concluir o cadastro de novos carros

--- cars/views.py
+++ cars/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from cars.models import Car
-
+from cars.forms import CarForm
 
 # Create your views here.
 def cars_view(request):
@@ -11,3 +11,10 @@ def cars_view(request):
     if search:
         cars = Car.objects.filter(model__icontains=search)
     return render(request,'cars.html', {'cars': cars })
+
+
+
+def new_car_view(request):
+    new_car_form = CarForm()
+    return render(request, 'new_car.html', {'new_car_form': new_car_form})
+
