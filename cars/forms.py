ab29cafd64bf9bd1493779c3d9ec4297ef9ad8d5
joao <vitordossantos99@hotmail.com>
2023-12-07 23:01:43 -0300

    Criação do forms e validação de formulario

--- cars/forms.py
+++ cars/forms.py
@@ -1,11 +1,31 @@
 from django import forms 
-from cars.models import Brand
+from cars.models import Car
+import re
 
-class CarForm(forms.Form):
-    model = forms.CharField(max_length=200)
-    brand = forms.ModelChoiceField(Brand.objects.all())
-    factory_year = forms.IntegerField ()
-    model_year = forms.IntegerField ()
-    plate = forms.CharField(max_length=10)
-    value = forms.FloatField()
-    photo = forms.ImageField()
+
+class CarlModelform(forms.ModelForm):
+    class Meta:
+        model = Car
+        fields = '__all__'
+
+    def clean_value(self):
+        value = self.cleaned_data.get('value')
+        if value <20000:
+            self.add_error('value', 'Valor minímo do carro deve ser de  R$ 20.000.')
+            return value
+        
+    def clean_factory_year(self):
+        factory_year = self.cleaned_data.get('factory_year')
+        if factory_year <1975:
+            self.add_error('factory_year','Não é possível cadastrar carros fabricados antes de 1975.')
+
+    
+    def clean_plate(self):
+        plate = self.cleaned_data.get('plate')
+        standard = re.compile (r'[A-Z][A-Z][A-Z][0-9][A-Z][0-9][0-9]')
+        check = standard.match (plate)
+
+        if check ==  None:
+            self.add_error('plate','Incorreto o formato da placa deve ser como: ABC1D23')
+            return plate
+        
--- cars/views.py
+++ cars/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from cars.models import Car
-from cars.forms import CarForm
+from cars.forms import CarlModelform
 
 # Create your views here.
 def cars_view(request):
@@ -10,11 +10,17 @@ def cars_view(request):
 
     if search:
         cars = Car.objects.filter(model__icontains=search)
-    return render(request,'cars.html', {'cars': cars })
+    return render(request,'cars.html', {'cars': cars }
+    )
 
 
 
 def new_car_view(request):
-    new_car_form = CarForm()
+    if request.method == 'POST':
+        new_car_form = CarlModelform(request.POST, request.FILES)
+        if new_car_form.is_valid():
+            new_car_form.save()
+            return redirect('cars_list')
+    else:
+        new_car_form = CarlModelform()
     return render(request, 'new_car.html', {'new_car_form': new_car_form})
-
