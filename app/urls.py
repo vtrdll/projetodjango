664407ba4ae184ac086be7ce684e4ab05b4ee684
joao <vitordossantos99@hotmail.com>
2023-12-09 19:28:49 -0300

    Inserção rotas de registro e autenticação

--- app/urls.py
+++ app/urls.py
@@ -19,11 +19,16 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from cars.views import cars_view, new_car_view
+from accounts.views import register_view, login_view, logout_view
+
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls), 
-    path('cars/',cars_view, name= 'cars_list'),
+    path ('register/',register_view, name='register'),
+    path ('logout', logout_view, name='logout'),
+    path ('login/', login_view, name='login'),
+    path ('cars/', cars_view, name= 'cars_list'),
     path('new_car/', new_car_view ,name= 'new_car'),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
